dc765c66c8f3edb393b644ac12d045127192d942
Joshua Baker <joshua.jbaker12@icloud.com>
2024-06-11 10:07:11 -0400

    first commit

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { ClerkMiddlewareOptions, clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware({
+    publicRoutes: [
+        '/',
+        '/events/:id',
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
+    ],
+    ignoredRoutes: [
+        '/api/webhook/clerk',
+        '/api/webhook/stripe',
+        '/api/uploadthing'
+    ]
+} as ClerkMiddlewareOptions);
+
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
