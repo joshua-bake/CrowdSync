af071cf4b8134abebf6ae00de59c9996a9eea87e
Joshua Baker <joshua.jbaker12@icloud.com>
2024-07-09 10:52:34 -0400

    added category, event, and order models

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,7 +1,8 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent, clerkClient } from '@clerk/nextjs/server'
-import { createUser } from '@/lib/actions/user.actions'
+import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions'
+import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
 
@@ -74,6 +75,30 @@ export async function POST(req: Request) {
                 }
             })
         }
+        return NextResponse.json({ message: 'OK', user: newUser })
+    }
+
+    if (eventType === 'user.updated') {
+        const { id, image_url, first_name, last_name, username } = evt.data;
+
+        const user = {
+            firstName: first_name!,
+            lastName: last_name!,
+            username: username!,
+            photo: image_url,
+        }
+
+        const updatedUser = await updateUser(id, user);
+
+        return NextResponse.json({ message: 'OK', user: updatedUser })
+    }
+
+    if (eventType === 'user.deleted') {
+        const { id } = evt.data;
+
+        const deletedUser = await deleteUser(id!);
+
+        return NextResponse.json({ message: 'OK', user: deletedUser })
     }
 
     return new Response('', { status: 200 })
